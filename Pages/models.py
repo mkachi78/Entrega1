ebce7513ea27c911a6d84329ef80cd2a4af2e203
Martin Kachinovsky <mkachi78@gmail.com>
2022-09-27 15:27:44 -0300

    ckeditor funcionando

--- Pages/models.py
+++ Pages/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
 
 class Pages (models.Model):
     author = models.OneToOneField(User, on_delete=models.CASCADE, unique=False)
     title = models.CharField(max_length=40)
     subject = models.CharField(max_length=40)
-    content = models.CharField(max_length=40)
+    content = RichTextField()
     date_create = models.DateField()
     imagen = models.ImageField()
--- ProyectoFinal/settings.py
+++ ProyectoFinal/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'sistem',
     'UserAdmin',
     'Messages',
-    'Pages'
+    'Pages',
+    'ckeditor'
 ]
 
 MIDDLEWARE = [
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
