39f56116103b57c23cdb20d9c9b24ada2fad0b38
Martin Kachinovsky <mkachi78@gmail.com>
2022-09-27 21:28:14 -0300

    correcciones a carga de imagen en page

--- Pages/forms.py
+++ Pages/forms.py
@@ -6,6 +6,6 @@ from ckeditor.widgets import CKEditorWidget
 class PagesForm(forms.Form):
     title = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     subject = forms.CharField(max_length=40, widget=forms.TextInput(attrs={'class': 'form-control'}))
-    imagen = forms.ImageField()
+    imagen = forms.ImageField(required=False)
     content = forms.CharField(widget=CKEditorWidget(attrs={'class': 'form-control'}))
     #content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control'}))
--- Pages/models.py
+++ Pages/models.py
@@ -2,9 +2,11 @@ from django.db import models
 
 from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
+from django.db.models import ForeignKey
+
 
 class Pages (models.Model):
-    author = models.OneToOneField(User, on_delete=models.CASCADE, unique=False)
+    author = ForeignKey(User, on_delete=models.CASCADE, related_name='author')
     title = models.CharField(max_length=40)
     subject = models.CharField(max_length=40)
     content = RichTextField()
